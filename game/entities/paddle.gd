3190ae35d9cd6a78b4ca3fc1e2ee9fd891444251
Chris Sprance <chrissprance@gmail.com>
2024-11-04 13:37:38 -0500

    Hook up paddle bounce signals

--- game/entities/paddle.gd
+++ game/entities/paddle.gd
@@ -2,5 +2,13 @@ class_name Paddle
 extends Entity
 
 
+@export var normal := Vector2(0, 1)  # Adjust based on your bumper's orientation
+
+
 func on_ready() -> void:
 	Utils.sync_transform(self)
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Entity:
+		var entity = body as Entity
+		entity.add_component(Bounced.new(normal))
