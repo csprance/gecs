88a421620410bc09713334c0ea364ee71f4f8202
Chris Sprance <chrissprance@gmail.com>
2024-11-04 13:16:30 -0500

    Now we need the Bounced component and whenever we bounce we remove the bounced component

--- game/systems/bounce_system.gd
+++ game/systems/bounce_system.gd
@@ -2,18 +2,21 @@ class_name BounceSystem
 extends System
 
 func _init():
-	required_components = [Transform, Velocity, Bounce]
+	required_components = [Transform, Velocity, Bounce, Bounced]
 
 func process(entity : Entity, delta: float):
 	# Get our bounce and velocity component
 	var bounce_component: Bounce = entity.get_component(Bounce)
+	var bounced_component: Bounced = entity.get_component(Bounced)
 	# If it should bounce
 	if bounce_component.should_bounce:
 		# Get the velocity componet and modify it
 		var velocity_component: Velocity = entity.get_component(Velocity)
-		# Take the direction from the bounce component
-		velocity_component.direction = bounce_component.normal
-		# TODO: Maybe we do some sort of reduction of speed here?
-		# It shoudl stop bouncing on the next frame
-		bounce_component.should_bounce = false
+		# Reflect the velocity direction over the normal
+		var incoming_direction = velocity_component.direction.normalized()
+		var normal = bounced_component.normal.normalized()
+		var reflected_direction = incoming_direction.bounce(normal)
+		velocity_component.direction = reflected_direction
+	# remove the bounced Component
+	entity.remove_component(Bounced)
 
