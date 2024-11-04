50c92d4b69e581c926b40306643622cf512d15ac
Chris Sprance <chrissprance@gmail.com>
2024-11-04 13:17:59 -0500

    Hook up glue code to Bumper so it adds a component to an entity.
    This seems like the most ECS/Godot way possible.
    
    Entities just do set up and glue code or add components.
    
    Systems control everything else

--- game/entities/bumper.gd
+++ game/entities/bumper.gd
@@ -1,21 +1,13 @@
 class_name Bumper
 extends Entity
 
+@export var normal := Vector2(0, -1)  # Adjust based on your bumper's orientation
+
 
 func on_ready() -> void:
 	Utils.sync_transform(self)
-
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print(body)
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	print(area) # Replace with function body.
-
-
-func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	print('') # Replace with function body.
-
-
-func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	print('') # Replace with function body.
+	if body is Entity:
+		var entity = body as Entity
+		entity.add_component(Bounced.new(normal))
