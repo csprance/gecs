7092627932509ba353d4ef274d239097680254a5
Chris Sprance <chrissprance@gmail.com>
2024-11-06 18:59:06 -0500

    Fix template

--- script_templates/Node/system.gd
+++ script_templates/Node/system.gd
@@ -4,7 +4,7 @@ extends System
 # Remember: Systems contain the meat and potatos of everything and can delete
 # themselves or add other systems etc. System order matters.
 
-func query(q: QueryBuilder) -> QueryBuilder:
+func query() -> QueryBuilder:
 	return q.with_all([]) # add required components
 	# process_empty = false # Do we want this to run every frame even with no entities?
 
