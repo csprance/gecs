61c5a1fad84a793106cca736cb39f45ed00b4df8
Chris Sprance <chrissprance@gmail.com>
2024-11-07 20:45:53 -0500

    improve the game. Keep paddle from going out of bounds. Fix collision system setup to change on specific types of colliders

--- game/systems/gameplay/s_player_movement.gd
+++ game/systems/gameplay/s_player_movement.gd
@@ -7,23 +7,28 @@ class_name PlayerMovementSystem
 extends System
 
 func query() -> QueryBuilder:
-	return q.with_all([C_Velocity, C_PlayerMovement])
+    return q.with_all([C_Velocity, C_PlayerMovement])
 
 
-func process(entity: Entity, _delta: float) -> void:
-	# Get the velocity component from the entity
-	var velocity = entity.get_component(C_Velocity) as C_Velocity
-	var movement = entity.get_component(C_PlayerMovement) as C_PlayerMovement
+func process(entity, delta: float) -> void:
+    var paddle = entity as Paddle
+    # Get the velocity component from the entity
+    var velocity = entity.get_component(C_Velocity) as C_Velocity
+    var movement = entity.get_component(C_PlayerMovement) as C_PlayerMovement
+    var trs = entity.get_component(C_Transform) as C_Transform
 
-	# Reset our movement
-	movement.axis = Vector2.ZERO
+    # Reset our movement
+    movement.axis = Vector2.ZERO
 
-	# Determine the move axis
-	if Input.is_action_pressed('paddle_left'):
-		movement.axis = Vector2.LEFT
-	elif Input.is_action_pressed('paddle_right'):
-		movement.axis = Vector2.RIGHT
+    # Determine the move axis
+    if Input.is_action_pressed('paddle_left'):
+        movement.axis = Vector2.LEFT
+    elif Input.is_action_pressed('paddle_right'):
+        movement.axis = Vector2.RIGHT
 
-	# Update velocity based the move axis and speed
-	velocity.direction = movement.axis
-	velocity.speed = movement.speed if movement.axis != Vector2.ZERO else 0.0
+    # if we collide with the wall, we stop moving
+    if paddle.test_move(Transform2D(trs.rotation, trs.position), movement.axis * movement.speed * delta):
+        movement.axis = Vector2.ZERO
+    # Update velocity based the move axis and speed
+    velocity.direction = movement.axis
+    velocity.speed = movement.speed if movement.axis != Vector2.ZERO else 0.0
--- game/systems/physics/s_collision.gd
+++ game/systems/physics/s_collision.gd
@@ -8,31 +8,49 @@ func query() -> QueryBuilder:
 
 func process(entity, _delta: float):
     var collision = entity.get_component(C_Collision).collision
-    
-    # The thing we collided with takes damage
     var collider = collision.get_collider()
-    collider.add_component(C_Damage.new())
+
+    # Default collision just adds a bounce and other collisons can modify that
+    var c_bounced = C_Bounced.new()
+    c_bounced.normal = collision.get_normal()
+    entity.add_component(c_bounced)
+
+    match collider.get_script():
+        Paddle:
+            paddle_collision(entity, collider, collision)  
+        Bumper:
+            bumper_collision(entity, collider, collision)
+        Brick:
+            brick_collision(entity, collider, collision)
+        _:
+            Loggie.error("Unknown collider type: ", collider.get_script())
     
+    entity.remove_component(C_Collision)
+
+# When things collide with a bumper, they get the components from the bumper
+func bumper_collision(entity, collider, _collision):
+    # They bounce off the bumper the same
+    for comp in collider.components_to_add:
+            entity.add_component(comp.duplicate())
+
+# When things collide with a paddle, they bounce off the paddle but based on the center of the paddle
+func paddle_collision(entity, collider, collision):
     # The thing that collides bounces off the collider surface
     var c_bounced = C_Bounced.new()
     c_bounced.normal = collision.get_normal()
+    var entity_trs = entity.get_component(C_Transform) as C_Transform
+    var paddle_trs = collider.get_component(C_Transform) as C_Transform
 
-    # if the collider is the paddle change the angle
-    if collider is Paddle:
-        var entity_trs = entity.get_component(C_Transform) as C_Transform
-        var paddle_trs = collider.get_component(C_Transform) as C_Transform
+    # Calculate the delta vector from paddle to ball
+    var half_width = collider.paddle_width / 2.0
+    var max_rot_rad = deg_to_rad(collider.max_rot)
+    var delta = remap(entity_trs.position.x - paddle_trs.position.x, -half_width, half_width, -max_rot_rad, max_rot_rad)
 
-        # Calculate the delta vector from paddle to ball
-        var half_width = collider.paddle_width / 2.0
-        var max_rot_rad = deg_to_rad(collider.max_rot)
-        var delta = remap(entity_trs.position.x - paddle_trs.position.x, -half_width, half_width, -max_rot_rad, max_rot_rad)
-
-        # Rotate the normal vector by the calculated angle
-        collider.last_normal = collision.get_normal().rotated(delta)
-        c_bounced.normal = collider.last_normal
-    
-    
+    # Rotate the normal vector by the calculated angle
+    collider.last_normal = collision.get_normal().rotated(delta)
+    c_bounced.normal = collider.last_normal
     entity.add_component(c_bounced)
-    entity.remove_component(C_Collision)
 
-    
+func brick_collision(_entity, collider, _collision):
+    # The thing we collided with takes damage
+    collider.add_component(C_Damage.new())
