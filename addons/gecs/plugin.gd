e394af522b04cac00fd4eed26f88fad1d0837380
Chris Sprance <chrissprance@gmail.com>
2024-11-22 16:03:56 -0500

    Add log level setting and refactor logger functionality

--- addons/gecs/plugin.gd
+++ addons/gecs/plugin.gd
@@ -1,7 +1,6 @@
 @tool
 extends EditorPlugin
 
-var setting_name = 'gecs/entity_base_type'
 
 func _enter_tree():
 	add_autoload_singleton("ECS", "res://addons/gecs/ecs.gd")
@@ -12,21 +11,7 @@ func _exit_tree():
 	# remove_gecs_project_setings()
 
 func _on_settings_changed():
-	var base_type = ProjectSettings.get(setting_name)
-	_apply_entity_base_type(base_type)
-
-func _apply_entity_base_type(base_type):
-	var entity_script_path = "res://addons/gecs/entity.gd"
-	var file = FileAccess.open(entity_script_path, FileAccess.READ_WRITE)
-	if file:
-		var lines = file.get_as_text().split("\n")
-		for i in lines.size():
-			if lines[i].begins_with("extends "):
-				lines[i] = "extends " + base_type
-				break
-		file.seek(0)
-		file.store_string('\n'.join(lines))
-		file.close()
+	pass
 
 ## Adds a new project setting to Godot.
 ## TODO: Figure out how to also add the documentation to the ProjectSetting so that it shows up 
