8fa1771873886452b8327b6ab5d5e296b4e95a0e
Chris Sprance <chrissprance@gmail.com>
2024-11-25 18:18:38 -0500

    Rename 'value' to 'text' in C_DebugLabel and update DebugLabel3DSystem to use the new property; add offset support for label positioning

--- game/lib/debug_label.gd
+++ game/lib/debug_label.gd
@@ -2,11 +2,14 @@
 class_name DebugLabel
 extends Sprite3D
 
-@export var debug_text: String = "Debug Label"
+## The text label to display
+@export var text: String = "Debug Label"
+
 @onready var label:  Label = %Label
 @onready var viewport: SubViewport = %SubViewport
 
+
 func _process(delta: float) -> void:
-    label.text = debug_text
+    label.text = text
     viewport.size = label.get_rect().size
     
--- game/systems/debug/s_debug_label_3d.gd
+++ game/systems/debug/s_debug_label_3d.gd
@@ -2,21 +2,23 @@ class_name DebugLabel3DSystem
 extends System
 
 var debug_labels = {}
-
+@export var debug_label_scene : PackedScene
 
 func query():
     return q.with_all([C_DebugLabel, C_Transform])
 
-func process(entity, delta):
+func process(entity, _delta):
+    var c_debug_label = entity.get_component(C_DebugLabel) as C_DebugLabel
     if debug_labels.has(entity):
         var label = debug_labels[entity]
-        label.text = entity.get_component(C_DebugLabel).value
-        label.global_transform.origin = entity.get_component(C_Transform).transform.origin
+        label.text = c_debug_label.text
+        label.global_transform.origin = entity.get_component(C_Transform).transform.origin + c_debug_label.offset
     else:
-        var label = create_label(entity)
+        var label = create_label(c_debug_label)
+        add_child(label)
         debug_labels[entity] = label
 
-func create_label(entity):
-    var label = DebugLabel.new()
-    label.text = entity.get_component(C_DebugLabel).value
+func create_label(c_debug_label: C_DebugLabel):
+    var label = debug_label_scene.instantiate() as DebugLabel
+    label.text = c_debug_label.text
     return label
