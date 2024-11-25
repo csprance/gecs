776868d6344d68db4519e9835bc13173cf76e946
Chris Sprance <chrissprance@gmail.com>
2024-11-25 02:24:11 -0500

    Add metadata export to Action class and update execute method warning

--- game/lib/action.gd
+++ game/lib/action.gd
@@ -1,6 +1,10 @@
 class_name Action
 extends Resource
 
+@export var meta = {
+    
+}
 
+## Always Override this with your own 
 func execute() -> void:
-    Loggie.debug('Action executed')
+    Loggie.warn('Default Action executed. You Should Probably Replace this!!')
