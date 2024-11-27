127033488a5d9209e2614d5e0deb24011d7229e9
Chris Sprance <chrissprance@gmail.com>
2024-11-26 19:01:48 -0500

    Add meta data to Action class for default action description

--- game/lib/action.gd
+++ game/lib/action.gd
@@ -1,8 +1,10 @@
 class_name Action
 extends Resource
 
+## Meta Data for the Action
 @export var meta = {
-    
+    'name': 'Default Action',
+    'description': 'This is the default action that is executed when no other action is assigned',
 }
 
 ## Always Override this with your own 
