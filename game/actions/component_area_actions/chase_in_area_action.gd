7194dc9aa6b092c7c6280a6d61afdac435364251
Chris Sprance <chrissprance@gmail.com>
2024-11-30 02:04:00 -0500

    Refactor chase_in_area_action to use relationships for chasing players

--- game/actions/component_area_actions/chase_in_area_action.gd
+++ game/actions/component_area_actions/chase_in_area_action.gd
@@ -5,14 +5,15 @@ extends ComponentAreaAction
 func on_enter(parent: Entity, body: Entity, _body_rid: RID, _body_shape_index: int, _local_shape_index: int) -> void:
 	if body is Player:
 		Loggie.debug('Started Chasing', body)
-		parent.add_component(C_Chasing.new(body))
+		parent.add_relationship(Relationship.new(C_IsChasing.new(), body))
 
 # Stop chasing the body if they get out of the interest area and become interested in the last position
 func on_exit(parent: Entity, body: Entity, _body_rid: RID, _body_shape_index: int, _local_shape_index: int) -> void:
 	if body is Player:
 		Loggie.debug('Stopped Chasing', body)
-	if parent.has_component(C_Chasing):
-		parent.remove_component(C_Chasing)
+	
+	if parent.has_relationship(Relationships.chasing_players()):
+		parent.remove_relationship(Relationships.chasing_players())
 		Loggie.debug('Interested in position now', body.global_transform.origin)
 		parent.add_component(C_Interested.new(body.global_transform.origin))
 		parent.add_component(C_LookAt.new(body.global_transform.origin))
