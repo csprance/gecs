34d43b529b991294481949e008e38abae27c3e62
Chris Sprance <chrissprance@gmail.com>
2024-11-30 02:04:06 -0500

    Add chasing_players relationship to manage player chasing behavior

--- game/lib/relationships.gd
+++ game/lib/relationships.gd
@@ -2,3 +2,6 @@ class_name Relationships
 
 static func attacking_players():
     return Relationship.new(C_IsAttacking.new(), Player)
+
+static func chasing_players():
+    return Relationship.new(C_IsChasing.new(), Player)
