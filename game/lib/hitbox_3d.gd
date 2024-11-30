136ba2e71d1d39e27f620302be8b91625a942723
Chris Sprance <chrissprance@gmail.com>
2024-11-30 12:13:00 -0500

    Refactor Hitbox3D to extend StaticBody3D; update hitbox_scale handling and debug drawing

--- game/lib/hitbox_3d.gd
+++ game/lib/hitbox_3d.gd
@@ -1,7 +1,7 @@
 ## This class specifically handles collisions with projectiles
 @tool
 class_name Hitbox3D
-extends Area3D
+extends StaticBody3D
 
 signal hitbox_entered(projectile: Projectile, parent: Entity, part: String)
 signal hitbox_exited(projectile: Projectile, parent: Entity, part: String)
@@ -12,10 +12,11 @@ signal hitbox_exited(projectile: Projectile, parent: Entity, part: String)
 ## What part of the entity this hitbox is
 @export var part : String = 'default'
 ## The scale of the hitbox collision shape
-@export var hitbox_scale: Vector3 :
+@export var hitbox_scale := Vector3.ONE:
 	get:
-		return collision_shape_3d.scale
+		return hitbox_scale
 	set(v):
+		hitbox_scale = v
 		if collision_shape_3d:
 			collision_shape_3d.scale = v
 @export var debug: bool = true
@@ -25,15 +26,10 @@ signal hitbox_exited(projectile: Projectile, parent: Entity, part: String)
 ## The collision shape of the hitbox
 @onready var collision_shape_3d: CollisionShape3D = %CollisionShape3D
 
-func _process(delta: float) -> void:
-	if Engine.is_editor_hint() and debug:
+func _ready() -> void:
+	hitbox_scale = hitbox_scale
+
+func _process(_delta: float) -> void:
+	if debug:
 		DebugDraw3D.draw_box_xf(collision_shape_3d.global_transform, color)
 
-# When Something enteres the hitbox
-func _on_hitbox_entered(_body_rid:RID, body, _body_shape_index:int, _local_shape_index:int) -> void:
-	if body is Projectile:
-		hitbox_entered.emit(body, parent, part)
-
-func _on_hitbox_exited(_body_rid:RID, body, _body_shape_index:int, _local_shape_index:int) -> void:
-	if body is Projectile:
-		hitbox_exited.emit(body, parent, part)
