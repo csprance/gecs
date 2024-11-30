0d05aec57233a8db3f0a45de8d427ea4f6a94c65
Chris Sprance <chrissprance@gmail.com>
2024-11-29 19:00:24 -0500

    Zombies only are interested in the last position if you were in there LOS first

--- game/actions/component_area_actions/zombie_interest_area_action.gd
+++ game/actions/component_area_actions/zombie_interest_area_action.gd
@@ -11,6 +11,8 @@ func on_enter(parent: Entity, body: Entity, _body_rid: RID, _body_shape_index: i
 func on_exit(parent: Entity, body: Entity, _body_rid: RID, _body_shape_index: int, _local_shape_index: int) -> void:
 	if body is Player:
 		Loggie.debug('Stopped Chasing', body)
+	if parent.has_component(C_Chasing):
 		parent.remove_component(C_Chasing)
 		Loggie.debug('Interested in position now', body.global_transform.origin)
 		parent.add_component(C_Interested.new(body.global_transform.origin))
+		parent.add_component(C_LookAt.new(body.global_transform.origin))
