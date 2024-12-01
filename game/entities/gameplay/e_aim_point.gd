47448f79a232847ccc582daa46155a603cbb9bcd
Chris Sprance <chrissprance@gmail.com>
2024-12-01 10:55:17 -0500

    revert aimpoint stuff

--- game/entities/gameplay/e_aim_point.gd
+++ game/entities/gameplay/e_aim_point.gd
@@ -1,13 +1,5 @@
-@tool
 class_name AimPoint
 extends Entity
 
-@onready var sprite_2d := $Sprite2D as Sprite2D
-@export var color :Color = Color(1, 1, 1, 1):
-    set(v):
-        color = v
-        change_color()
-
-# Change color of the Sprite2D modulate
-func change_color():
-    sprite_2d.modulate = color
+@onready var sprite_2d := %Sprite2D as Sprite2D
+@export var color :Color = Color(1, 1, 1, 1)
