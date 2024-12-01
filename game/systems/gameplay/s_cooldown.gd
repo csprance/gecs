af058f7e5831d27edd978e4c38cdd7b395444227
Chris Sprance <chrissprance@gmail.com>
2024-12-01 12:08:40 -0500

    Fix logic  on cooldown system

--- game/systems/gameplay/s_cooldown.gd
+++ game/systems/gameplay/s_cooldown.gd
@@ -2,20 +2,17 @@
 class_name CooldownSystem
 extends System
 
-var coooldown_components = [C_AttackCooldown, C_RangedAttackCooldown]
+var coooldown_components: Array = [C_AttackCooldown, C_RangedAttackCooldown]
 
 func query() -> QueryBuilder:
-    return q.with_all(coooldown_components)
+    return q.with_any(coooldown_components)
 
 
 func process(entity: Entity, delta: float):
     for component in coooldown_components:
         if entity.has_component(component):
-            update_cooldown(entity.get_component(component), entity, delta)
-
-
-func update_cooldown(cooldown: Component, entity: Entity, delta: float):
-    cooldown.time -= delta
-    if cooldown.time <= 0:
-        # remove_component takes the class type not the instance so get that
-        entity.remove_component(cooldown.get_script())
+            var c_cooldown = entity.get_component(component)
+            c_cooldown.time -= delta
+            if c_cooldown.time <= 0:
+                # remove_component takes the class type not the instance so get that
+                entity.remove_component(component)
