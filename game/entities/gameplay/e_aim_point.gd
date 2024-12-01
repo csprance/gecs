a4a1cc24c64891a9128629b47c4820278ff7fab7
Chris Sprance <chrissprance@gmail.com>
2024-12-01 10:25:32 -0500

    Add color property to AimPoint and update Sprite2D modulation

--- game/entities/gameplay/e_aim_point.gd
+++ game/entities/gameplay/e_aim_point.gd
@@ -1,7 +1,13 @@
+@tool
 class_name AimPoint
 extends Entity
 
-@onready var sprite_2d := %Sprite2D as Sprite2D
+@onready var sprite_2d := $Sprite2D as Sprite2D
+@export var color :Color = Color(1, 1, 1, 1):
+    set(v):
+        color = v
+        change_color()
 
-
-
+# Change color of the Sprite2D modulate
+func change_color():
+    sprite_2d.modulate = color
