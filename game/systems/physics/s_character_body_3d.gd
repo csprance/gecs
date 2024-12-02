a8e988dedd37a30505d3672f28f5cf6105cdd6fd
Chris Sprance <chrissprance@gmail.com>
2024-12-02 14:05:43 -0500

    Refactor character body physics processing to improve collision handling and streamline velocity updates

--- game/systems/physics/s_character_body_3d.gd
+++ game/systems/physics/s_character_body_3d.gd
@@ -12,18 +12,19 @@ func process_all(entities, _delta: float):
 		if not entities[i] is CharacterBody3D:
 			continue # skip if it's not a character body
 		# Set the velocity from the velocity component
-		entities[i].velocity = velocitys[i].direction.normalized() * velocitys[i].speed
+		entities[i].velocity = (velocitys[i].direction.normalized() * velocitys[i].speed) 
 		# Move the entity
 		if entities[i].move_and_slide():
 			# Check if we're on the floor and ignore the floor collisions
-			# Add a collision event to the entity that just collided to handle collisions
-			var c_collision = C_Collision.new()
-			var col = entities[i].get_last_slide_collision()
-			c_collision.collision = col
-			entities[i].add_component(c_collision)
+			if not entities[i].is_on_floor():
+				pass
+				# # Add a collision event to the entity that just collided to handle collisions
+				# var c_collision = C_Collision.new()
+				# var col = entities[i].get_last_slide_collision()
+				# c_collision.collision = col
+				# entities[i].add_component(c_collision)
 		# Set the velocity from the entity to the component
 		velocitys[i].direction = entities[i].velocity.normalized()
 		velocitys[i].speed = entities[i].velocity.length()
 		# Sync the transform back to the entity
 		Utils.sync_transform(entities[i])
-
