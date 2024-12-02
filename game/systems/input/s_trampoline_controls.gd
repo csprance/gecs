6f8f3c876b5122715796c5c9087c41def0b5f449
Chris Sprance <chrissprance@gmail.com>
2024-12-02 14:03:26 -0500

    Enhance trampoline functionality with new exit locators and input controls

--- game/systems/input/s_trampoline_controls.gd
+++ game/systems/input/s_trampoline_controls.gd
@@ -2,9 +2,24 @@ class_name TrampolineControlsSystem
 extends System
 
 func query() -> QueryBuilder:
-    return q.with_all([C_TrampolineControls, C_Player]).with_relationship([Relationship.new(C_BouncingOn, Trampoline)])
+    return q\
+    .with_all([C_TrampolineControls, C_Player])\
+    .with_relationship([Relationship.new(C_BouncingOn.new(), Trampoline)])
 
 
 func process(entity, delta):
+    var c_velocity = entity.get_component(C_Velocity) as C_Velocity
     # Jump in the direction we press a key in
-    pass
+    if Input.is_action_just_pressed('move_down'):
+        c_velocity.direction = Vector3(0, -5, -1)
+        c_velocity.speed = 10
+    if Input.is_action_just_pressed('move_up'):
+        c_velocity.direction = Vector3(0, -5, 1)
+        c_velocity.speed = 10
+    if Input.is_action_just_pressed('move_left'):
+        c_velocity.direction = Vector3(-1, -5, 0)
+        c_velocity.speed = 10
+    if Input.is_action_just_pressed('move_right'):
+        c_velocity.direction = Vector3(1, -5, 0)
+        c_velocity.speed = 10
+    
