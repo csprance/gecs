b3f6de7c80ca22872e741c2e6283c2bc5d88a836
Chris Sprance <chrissprance@gmail.com>
2024-12-02 14:05:08 -0500

    Refactor VisualsSystem to ensure entities with C_Visuals do not already have C_HasVisuals

--- game/systems/gameplay/s_visuals.gd
+++ game/systems/gameplay/s_visuals.gd
@@ -1,13 +1,13 @@
+## This system is responsible for creating visuals for entities that have a C_Visuals component
+## it adds the visuals and then adds the has visuals component to the entity
 class_name VisualsSystem
 extends System
 
 func query():
-    return q.with_all([C_Visuals])
+    return q.with_all([C_Visuals]).with_none([C_HasVisuals])
 
 func process(entity, _delta: float):
     var c_visuals = entity.get_component(C_Visuals) as C_Visuals
     entity.add_child(c_visuals.packed_scene.instantiate())
-    # Remove the visuals component from the entity. and add a C_HasVisuals component
-    entity.remove_component(C_Visuals)
     
     entity.add_component(C_HasVisuals.new())
