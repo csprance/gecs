a8c1ebbb6d28e47c76e6f4d52b0cde12a2d3d094
Chris Sprance <chrissprance@gmail.com>
2024-12-03 00:33:01 -0500

    Add LevelResource class to define level properties and metadata

---
+++ game/lib/level_resource.gd
@@ -0,0 +1,11 @@
+class_name LevelResource
+extends Resource
+
+## The name of the level
+@export var level_name: String = ""
+## The quick description of the level
+@export var level_description: String = ""
+## The scene for the level
+@export var packed_scene: PackedScene
+## The password for the level you are shown after beating it. If empty this level has no password
+@export var password: String = ''
