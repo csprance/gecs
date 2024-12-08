c6d8848d18c36c4790aab560bb6d4a01f74dcadb
Chris Sprance <chrissprance@gmail.com>
2024-12-08 14:15:47 -0500

    each shot reduce the qty of a weapon now

--- game/actions/a_shoot_active_weapon.gd
+++ game/actions/a_shoot_active_weapon.gd
@@ -8,8 +8,17 @@ func execute(_e) -> void:
     if not active_weapon or not player:
         return
 
+    var c_qty = active_weapon.get_component(C_Quantity) as C_Quantity
+    if c_qty:
+        c_qty.value -= 1
+        if c_qty.value == 0:
+            InventoryUtils.remove_inventory_item(active_weapon)
+            return
+
     var direction = Utils.calculate_entity_direction(player)
     var projectile_transform = WeaponUtils.create_projectile_transform(player, direction)
         # Retrieve the projectile component from the active weapon.
     var c_projectile = WeaponUtils.get_projectile_component(active_weapon)
     WeaponUtils.instantiate_projectile(c_projectile, projectile_transform)
+
+    GameState.weapon_fired.emit(active_weapon)
