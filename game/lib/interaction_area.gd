4f91589813a3b3493a7c4943a1e3d84dec6cfb39
Chris Sprance <chrissprance@gmail.com>
2024-12-09 16:23:12 -0500

    Refactor interaction area query handling; extract query logic into separate function and enhance area exit checks

--- game/lib/interaction_area.gd
+++ game/lib/interaction_area.gd
@@ -6,6 +6,12 @@ extends Area3D
 ## The static queries that the interaction area will use to check if an entity can interact with it
 @export var can_interact_query: Array[StaticQuery]
 
+func get_can_interact_query() -> QueryBuilder:
+    var query: QueryBuilder = ECS.world.query
+    # Combine all static queries
+    for static_query in can_interact_query:
+        query.combine(static_query.query())
+    return query
 
 func _ready():
     # Connect the area_entered signal to the parent entity
@@ -15,14 +21,12 @@ func _ready():
 
 func _on_area_entered(_body_id: RID, body, _body_shape: int, _area_shape: int) -> void:
     if body is Entity:
-        var query: QueryBuilder = ECS.world.query
-        # Combine all static queries
-        for static_query in can_interact_query:
-            query.combine(static_query.query())
         # Check if the body matches the query
-        if not query.matches([body]).is_empty():
+        if not get_can_interact_query().matches([body]).is_empty():
             body.add_relationship(Relationship.new(C_CanInteractWith.new(), parent))
 
 func _on_area_exited(_body_id: RID, body, _body_shape: int, _area_shape: int) -> void:
-    if parent:
-        body.remove_relationship(Relationship.new(C_CanInteractWith.new(), parent))
+    if parent and body and body is Entity:
+        # Check if the body matches the query
+        if not  get_can_interact_query().matches([body]).is_empty():
+            body.remove_relationship(Relationship.new(C_CanInteractWith.new(), parent))
