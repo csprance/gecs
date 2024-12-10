293a4ec844864aa007ab6b6937726950fb76be2d
Chris Sprance <chrissprance@gmail.com>
2024-12-10 16:51:31 -0500

    Add explosion radius handling to Projectile; implement collision shape and area for explosion effects.

--- game/entities/gameplay/e_projectile.gd
+++ game/entities/gameplay/e_projectile.gd
@@ -1,6 +1,14 @@
 class_name Projectile
 extends Entity
 
+## This is used to check the actual projectile collision it is resized in [method WeaponUtils.instantiate_projectile]
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+## This is used to find all bodies in the explosion radius 
+@onready var explosion_radius: Area3D = %ExplosionRadius
+## This is the size of the explosion radius mapped onto a collision shape 3d it is resized in [method WeaponUtils.instantiate_projectile]
+@onready var explosion_radius_shape_3d: CollisionShape3D = %ExplosionRadiusShape3D
+
+
 func on_ready():
-    # Take the C_Transform and sync it with the transform of the entity
-    Utils.sync_from_transform(self)
+	# Take the C_Transform and sync it with the transform of the entity
+	Utils.sync_from_transform(self)
