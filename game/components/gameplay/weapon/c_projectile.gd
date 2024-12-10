3422855076c82d1423503a5e2015de184fc37edb
Chris Sprance <chrissprance@gmail.com>
2024-12-10 16:52:05 -0500

    Enhance C_Projectile component with additional properties for collision radius, lifetime, gravity effect, explosive radius, initial velocity, and impact effect.

--- game/components/gameplay/weapon/c_projectile.gd
+++ game/components/gameplay/weapon/c_projectile.gd
@@ -1,3 +1,6 @@
+# The C_Projectile component defines the properties and behavior of a projectile.
+# It specifies attributes such as visuals, damage, speed, collision scale, and lifetime.
+# This component is added to entities to make them function as projectiles in the game.
 class_name C_Projectile
 extends Component
 
@@ -6,10 +9,16 @@ extends Component
 ## When this hits something what component does it add to the hit entity
 @export var damage_component: Component
 ## How big the collision sphere is
-@export var collision_scale:= Vector3.ONE
+@export var collision_radius:= 0.1
 ## How fast this moves
 @export var speed:= 10.0
-## How many hitboxes can we go through
-@export var pass_through_hitboxes:= 1
-
-var cur_pass_through_hitboxes:= 0
+## How long this lasts
+@export var lifetime: float = 5.0
+## Does this projectile get affected by gravity
+@export var affected_by_gravity: bool = true
+## How wide the radius of the explosion is
+@export var explosive_radius: float = 0.0
+## Some initial velocity
+@export var initial_velocity: Vector3 = Vector3.ZERO
+## The impact effect to spawn when this hits something
+@export var impact_effect: PackedScene = null
