9d90c180eb4d8fa92b5e9f5817c8f7bb417195e7
Chris Sprance <chrissprance@gmail.com>
2024-12-10 16:51:57 -0500

    Add C_Lifetime component to manage entity lifespan; implement initialization method for customizable lifetime duration.

--- game/components/gameplay/c_lifetime.gd
+++ game/components/gameplay/c_lifetime.gd
@@ -1,4 +1,10 @@
+# The C_Lifetime component manages the lifespan of an entity.
+# It ensures that entities are removed from the game after a specified duration.
+# This component is used for entities like projectiles or temporary effects that should not persist indefinitely.
 class_name C_Lifetime
 extends Component
 
 @export var lifetime: float = 1.0
+
+func _init(_lifetime=1.0) -> void:
+    lifetime = _lifetime
