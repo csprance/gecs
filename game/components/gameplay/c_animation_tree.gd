4d5e1e8c9f9a88de705eaa4335b7076e0dd603e6
Chris Sprance <chrissprance@gmail.com>
2024-12-12 09:14:56 -0500

    Add animation tree components and refactor animation player system for improved functionality with animation trees

---
+++ game/components/gameplay/c_animation_tree.gd
@@ -0,0 +1,6 @@
+## References an animation Tree 
+class_name C_AnimationTree
+extends Component
+
+## The path to the animation player node
+@export var anim_tree: NodePath
--- game/systems/gameplay/s_animation_player.gd
+++ game/systems/gameplay/s_animation_player.gd
@@ -2,22 +2,33 @@
 class_name AnimationPlayerSystem
 extends System
 
-func query():
-    return q.with_all([C_AnimationPlayer, C_PlayAnimation])
+func sub_systems():
+	return [
+		[ECS.world.query.with_all([C_AnimationPlayer, C_PlayAnimation]), anim_player_subsy],
+		[ECS.world.query.with_all([C_AnimationTree, C_AnimTreeCommand]), anim_tree_subsy],
+	]
 
-func process(entity: Entity, delta: float) -> void:
-    var c_play_animation = entity.get_component(C_PlayAnimation) as C_PlayAnimation
-    if c_play_animation.time == 0.0:
-        var c_anim_player: C_AnimationPlayer = entity.get_component(C_AnimationPlayer)
-        var anim_player = entity.get_node(c_anim_player.player) as AnimationPlayer
-        anim_player.play(c_play_animation.anim_name, c_play_animation.time)
+func anim_tree_subsy(entity: Entity, delta: float) -> void:
+	var c_anim_tree = entity.get_component(C_AnimationTree) as C_AnimationTree
+	var c_anim_tree_cmd = entity.get_component(C_AnimTreeCommand) as C_AnimTreeCommand
+	var anim_tree = entity.get_node(c_anim_tree.anim_tree) as AnimationTree
+	var state_machine = anim_tree["parameters/playback"]
+	state_machine.travel(c_anim_tree_cmd.command)
 
-    c_play_animation.time += delta * c_play_animation.anim_speed
-    
-    if c_play_animation.time >= 1.0:
-        c_play_animation.finished = true
-        c_play_animation.time = 0.0
-        if c_play_animation.callback:
-            c_play_animation.callback.call()
-        if not c_play_animation.loop:
-            entity.remove_component(C_PlayAnimation)
+
+func anim_player_subsy(entity: Entity, delta: float) -> void:
+	var c_play_animation = entity.get_component(C_PlayAnimation) as C_PlayAnimation
+	if c_play_animation.time == 0.0:
+		var c_anim_player: C_AnimationPlayer = entity.get_component(C_AnimationPlayer)
+		var anim_player = entity.get_node(c_anim_player.player) as AnimationPlayer
+		anim_player.play(c_play_animation.anim_name, c_play_animation.time)
+
+	c_play_animation.time += delta * c_play_animation.anim_speed
+	
+	if c_play_animation.time >= 1.0:
+		c_play_animation.finished = true
+		c_play_animation.time = 0.0
+		if c_play_animation.callback:
+			c_play_animation.callback.call()
+		if not c_play_animation.loop:
+			entity.remove_component(C_PlayAnimation)
