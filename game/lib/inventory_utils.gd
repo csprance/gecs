bc2b1484504a7e88e391261c79c49e493ed3e27c
Chris Sprance <chrissprance@gmail.com>
2024-12-13 18:23:19 -0500

    Refactor inventory pickup logic to streamline item and weapon handling

--- game/lib/inventory_utils.gd
+++ game/lib/inventory_utils.gd
@@ -21,14 +21,40 @@ static func use_inventory_item(item: Entity, player: Entity):
 ##   - quantity: The quantity of the item to add.
 ## Returns:
 ##   - The new item entity added to the inventory.
-static func add_inventory_c_item(c_item: C_Item, quantity: int = 1):
-	var new_item = Item.new()
-	new_item.add_components([c_item, C_InInventory.new(), C_Quantity.new(quantity)])
+static func pickup_weapon(pickup: Pickup):
+	var player = pickup.get_relationship(Relationship.new(C_OwnedBy.new(), Player)).target
+	assert(player, 'Player not found')
+	if pickup.weapon_resource.pickup_action:
+		pickup.weapon_resource.pickup_action.run_action()
+	var new_weapon = Entity.new()
+	new_weapon.name = pickup.weapon_resource.name
+	new_weapon.add_components([pickup.weapon_resource, C_InInventory.new(), C_Quantity.new(pickup.quantity)])
+	new_weapon.add_relationship(Relationship.new(C_OwnedBy.new(), player))
+	Loggie.debug('Added item to inventory: ', new_weapon.name, ' Quantity: ', pickup.quantity)
+	ECS.world.add_entity(new_weapon)
+	GameState.inventory_weapon_added.emit(new_weapon)
+	if not GameState.active_weapon:
+		GameState.active_weapon = new_weapon
+	
+	return new_weapon
+
+static func pickup_item(pickup: Pickup):
+	var player = pickup.get_relationship(Relationship.new(C_OwnedBy.new(), Player)).target
+	assert(player, 'Player not found')
+	if pickup.item_resource.pickup_action:
+		pickup.item_resource.pickup_action.run_action()
+	var new_item = Entity.new()
+	new_item.name = '-'.join([pickup.item_resource.name, pickup.get_instance_id()])
+	new_item.add_components([pickup.item_resource, C_InInventory.new(), C_Quantity.new(pickup.quantity)])
+	new_item.add_relationship(Relationship.new(C_OwnedBy.new(), player))
 	ECS.world.add_entity(new_item)
 	GameState.inventory_item_added.emit(new_item)
-	Loggie.debug('Added item to inventory: ', new_item.name, ' Quantity: ', quantity)
+	Loggie.debug('Added item to inventory: ', new_item.name, ' Quantity: ', pickup.quantity)
+	GameState.inventory_item_added.emit(new_item)
+	if not GameState.active_item:
+		GameState.active_item = new_item
+	
 	return new_item
-
 ## Gets the quantity of the specified item.
 ## Parameters:
 ##   - item: The item entity.
@@ -83,6 +109,7 @@ static func remove_inventory_item(item: Entity, remove_quantity = 1):
 			GameState.player.remove_component(C_HasActiveItem)
 
 		Loggie.debug('Removing Item', c_item_weapon)
+		GameState.inventory_item_removed.emit(item)
 	else:
 		Loggie.debug('Item does not have a C_Item component')
 
