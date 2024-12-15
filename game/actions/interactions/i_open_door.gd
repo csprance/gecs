e116abd60a0d4d570e07acdb4eb29b31f20725e0
Chris Sprance <chrissprance@gmail.com>
2024-12-15 12:08:02 -0500

    Implement key usage for door interactions and remove key from inventory upon opening

--- game/actions/interactions/i_open_door.gd
+++ game/actions/interactions/i_open_door.gd
@@ -1,13 +1,27 @@
 class_name OpenDoorInteraction
 extends Interaction
 
+@export var key: C_Item
+
 func _interaction(interactable: Entity, interactors: Array, meta: Dictionary = {}) -> bool:
-    # Make sure we're a door. 
+    # check to see if at least one of the interactors has a key
+    var e_key: Entity
+    for i in interactors:
+        e_key = InventoryUtils.get_item(i, key)
+        if e_key:
+            break
+    
+    if not e_key:
+        Loggie.debug("No key found")
+        return false
+    
     # play an animation to open the door
     interactable.add_component(C_PlayAnimation.new("open_door"))
     for i in interactors:
         i.remove_component(C_Interacting)
      # doors only work once
     interactable.remove_component(C_Interactable)
+    # remove the key from the key bearer
+    InventoryUtils.remove_inventory_item(e_key, 1)
     return true
     
