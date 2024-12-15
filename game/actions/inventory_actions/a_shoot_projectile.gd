019a047aea81e06677740730592e426930799b7d
Chris Sprance <chrissprance@gmail.com>
2024-12-15 17:33:24 -0500

    Enhance projectile shooting logic by adding player's velocity to instantiated projectiles

--- game/actions/inventory_actions/a_shoot_projectile.gd
+++ game/actions/inventory_actions/a_shoot_projectile.gd
@@ -13,7 +13,12 @@ func _use_item(active_weapon: Entity, player: Entity) -> void:
 	var projectile_transform = WeaponUtils.create_projectile_transform(player, direction)
 		# Retrieve the projectile component from the active weapon.
 	var c_projectile = WeaponUtils.get_projectile_component(active_weapon)
-	WeaponUtils.instantiate_projectile(c_projectile, projectile_transform)
+	var e_projectile = WeaponUtils.instantiate_projectile(c_projectile, projectile_transform)
+	# add the player's velocity to the projectile ( So we can't run into it)
+	var projectile_c_vel = e_projectile.get_component(C_Velocity) as C_Velocity
+	var player_c_vel = player.get_component(C_Velocity) as C_Velocity
+	projectile_c_vel.velocity += player_c_vel.velocity
+
 	var c_play_anim = C_PlayAnimation.new("player/shoot")
 	c_play_anim.callback = func (): player.add_component(C_PlayAnimation.new("player/idle", 1, true))
 	player.add_component(c_play_anim)
