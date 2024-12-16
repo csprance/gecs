d360b3ddc8bcfac2007bcaaeb9292f66754082d4
Chris Sprance <chrissprance@gmail.com>
2024-12-16 10:19:09 -0500

    Update gear script to conditionally remove default skeleton based on editor context

--- game/lib/gear.gd
+++ game/lib/gear.gd
@@ -9,8 +9,12 @@ var skeleton_path: NodePath
 
 
 func _ready() -> void:
-	remove_child(default_skeleton)
-	for child in find_children('*', 'BoneAttachment3D'):
-		var bone_attach: BoneAttachment3D = child
-		bone_attach.set_external_skeleton(skeleton_path)
-		bone_attach.set_use_external_skeleton(true)		
+	# When in game we want to remove the default skeleton and set the external skeleton based on the
+	# skeleton path which is set by the [GearSystem]
+	# In the editor we want to be able to see the default skeleton
+	if not Engine.is_editor_hint():
+		remove_child(default_skeleton)
+		for child in find_children('*', 'BoneAttachment3D'):
+			var bone_attach: BoneAttachment3D = child
+			bone_attach.set_external_skeleton(skeleton_path)
+			bone_attach.set_use_external_skeleton(true)		
