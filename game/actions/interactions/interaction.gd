06f56dd895420836377bc4e1a8d0888dc86c937e
Chris Sprance <chrissprance@gmail.com>
2024-12-17 20:49:58 -0500

    remove interacting components before executing interaction logic

--- game/actions/interactions/interaction.gd
+++ game/actions/interactions/interaction.gd
@@ -5,14 +5,16 @@ extends Action
 ## This method should be overidden in your own action to handle the interaction it is called when the interaction is triggered.
 ## It should return true if the interaction was successful and false if it was not.
 func _interaction(interactable: Entity, interactors: Array, meta: Dictionary = {}) -> bool:
-    assert(false, "You need to implement the interaction method in your interaction action")
-    return false
+	assert(false, "You need to implement the interaction method in your interaction action")
+	return false
 
 ## This method is called by the interaction system to run the Interaction. It should not be overidden and is used to intercept the interaction and pass it to the interaction method.
 ## It returns true if the interaction was successful and false if it was not.
 func run_interaction(interactable: Entity, interactors: Array, meta: Dictionary = {}) -> bool:
-    return _interaction(interactable, interactors, meta)
+	for interactor in interactors:
+		interactor.remove_component(C_Interacting)
+	return _interaction(interactable, interactors, meta)
 
 
 func run_action(_e=[], _m=[]) -> void:
-    assert(false, 'You should instead run the run_interaction for an interaction')
+	assert(false, 'You should instead run the run_interaction for an interaction')
