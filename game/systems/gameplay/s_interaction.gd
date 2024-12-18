d6f422a3fc9d617d8ee6be9745fd26f30f55651e
Chris Sprance <chrissprance@gmail.com>
2024-12-17 20:51:41 -0500

    Refactor interaction process for improved readability and maintainability

--- game/systems/gameplay/s_interaction.gd
+++ game/systems/gameplay/s_interaction.gd
@@ -7,23 +7,23 @@ extends System
 
 
 func query() -> QueryBuilder:
-    return q.with_all([C_Interactor]).with_relationship([Relationship.new(C_CanInteractWith.new(), ECS.wildcard)]).with_none([C_Interacting])
+	return q.with_all([C_Interactor]).with_relationship([Relationship.new(C_CanInteractWith.new(), ECS.wildcard)]).with_none([C_Interacting])
 
 
 func process(interactor: Entity, delta: float) -> void:
-    # if the interactor pressed the interact button start the interaction process
-    if Input.is_action_just_pressed('interact'):
-        # Get all the entities that the interactor can interact with
-        var r_interactables = interactor.get_relationships(Relationship.new(C_CanInteractWith.new(), ECS.wildcard))
-        for r in r_interactables:
-            var interactable = r.target
-            # FIXME: This isn't being added to the interactable's relationships
-            # Add the being interacted with relationship to the interactable with the interactor
-            interactable.add_relationship(Relationship.new(C_BeingInteractedWith.new(), interactor))
-            # This kicks it over to the interactables system to run the interaction
-            # remove the can interact with relationship
-            interactor.remove_relationship(r)
-            # specify we're interacting with the interactable
-            interactor.add_component(C_Interacting.new())
-    
-    
+	# if the interactor pressed the interact button start the interaction process
+	if Input.is_action_just_pressed('interact'):
+		# Get all the entities that the interactor can interact with
+		var r_interactables = interactor.get_relationships(Relationship.new(C_CanInteractWith.new(), ECS.wildcard))
+		for r in r_interactables:
+			var interactable = r.target
+			# FIXME: This isn't being added to the interactable's relationships
+			# Add the being interacted with relationship to the interactable with the interactor
+			interactable.add_relationship(Relationship.new(C_BeingInteractedWith.new(), interactor))
+			# This kicks it over to the interactables system to run the interaction
+			# remove the can interact with relationship
+			interactor.remove_relationship(r)
+			# specify we're interacting with the interactable
+			interactor.add_component(C_Interacting.new())
+	
+	
