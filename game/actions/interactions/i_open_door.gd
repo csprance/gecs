a36184906f4730de11764a6cabd89b020759b7d9
Chris Sprance <chrissprance@gmail.com>
2024-12-17 20:50:11 -0500

    Add locked state to OpenDoorInteraction and handle key removal

--- game/actions/interactions/i_open_door.gd
+++ game/actions/interactions/i_open_door.gd
@@ -1,21 +1,27 @@
 class_name OpenDoorInteraction
 extends Interaction
 
+@export var locked := false
+## Define the item it requires to open this. It could be any item
 @export_file("*.tres") var key_path
 
 func _interaction(interactable: Entity, interactors: Array, meta: Dictionary = {}) -> bool:
-	var key = load(key_path)
-	assert(key, "Key Type on Door Interation is not set or invalid on Entity: %s" % interactable)
-	# check to see if at least one of the interactors has a key
-	var e_key: Entity
-	for i in interactors:
-		e_key = InventoryUtils.get_item(i, key)
-		if e_key:
-			break
-	
-	if not e_key:
-		Loggie.debug("No key found")
-		return false
+	if locked:
+		var key = load(key_path)
+		assert(key, "Key Type on Door Interation is not set or invalid on Entity: %s" % interactable)
+		# check to see if at least one of the interactors has a key
+		var e_key: Entity
+		for i in interactors:
+			e_key = InventoryUtils.get_item(i, key)
+			if e_key:
+				break
+		
+		if not e_key:
+			Loggie.debug("No key found")
+			return false
+		
+		# remove the key from the world
+		InventoryUtils.remove_inventory_item(e_key, 1)
 	
 	# play an animation to open the door
 	interactable.add_component(C_PlayAnimation.new("open_door"))
@@ -23,7 +29,5 @@ func _interaction(interactable: Entity, interactors: Array, meta: Dictionary = {
 		i.remove_component(C_Interacting)
 	 # doors only work once
 	interactable.remove_component(C_Interactable)
-	# remove the key from the world
-	InventoryUtils.remove_inventory_item(e_key, 1)
 	return true
 	
--- game/actions/interactions/i_open_searchable.gd
+++ game/actions/interactions/i_open_searchable.gd
@@ -3,13 +3,13 @@ class_name OpenSearchableInteraction
 extends Interaction
 
 func _interaction(interactable: Entity, interactors: Array, meta: Dictionary = {}) -> bool:
-    # Make sure we're a door. 
-    # play an animation to open the door
-    interactable.add_component(C_PlayAnimation.new("open"))
-    for i in interactors:
-        i.remove_component(C_Interacting)
-    # searchables only work once
-    interactable.remove_component(C_Interactable)
-    # drop the item
-    
-    return true
+	# Make sure we're a door. 
+	# play an animation to open the door
+	interactable.add_component(C_PlayAnimation.new("open"))
+	for i in interactors:
+		i.remove_component(C_Interacting)
+	# searchables only work once
+	interactable.remove_component(C_Interactable)
+	# drop the item
+	
+	return true
