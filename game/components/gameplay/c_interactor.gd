c0cdd6b4c6bebcc8eedcc99300864719fe56ca9c
Chris Sprance <chrissprance@gmail.com>
2024-12-22 08:51:58 -0500

    Add interact_timer variable to C_Interactor for tracking interaction duration

--- game/components/gameplay/c_interactor.gd
+++ game/components/gameplay/c_interactor.gd
@@ -1,2 +1,5 @@
 class_name C_Interactor
 extends Component
+
+## This is used to store the time we are interacting with an entity, whether it's a a press, hold or rapid press interaction.
+var interact_timer: float = 0
