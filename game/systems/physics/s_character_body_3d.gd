fa58a7e290c913551d5a7cb71affafa9cb16edc1
Chris Sprance <chrissprance@gmail.com>
2024-12-22 08:53:56 -0500

    Refactor character body physics: rename variables for clarity and improve component handling in process_all function

--- game/systems/physics/s_character_body_3d.gd
+++ game/systems/physics/s_character_body_3d.gd
@@ -7,24 +7,27 @@ func query() -> QueryBuilder:
 	return q.with_all([C_Velocity, C_CharacterBody3D, C_Transform]).with_none([C_Projectile])
 
 func process_all(entities, _delta: float):
-	var velocitys = ECS.get_components(entities, C_Velocity) as Array[C_Velocity]
+	var c_velocitys = ECS.get_components(entities, C_Velocity) as Array[C_Velocity]
+	var c_trss = ECS.get_components(entities, C_Transform) as Array[C_Transform]
 	for i in range(entities.size()):
 		assert(entities[i] is CharacterBody3D, 'Entity is not a CharacterBody3D. Check its components')
 		if not entities[i] is CharacterBody3D:
 			continue # skip if it's not a character body
 		# Set the velocity from the velocity component
-		entities[i].velocity = velocitys[i].velocity
+		entities[i].velocity = c_velocitys[i].velocity
 		# Move the entity
 		if entities[i].move_and_slide():
+			pass
 			# Check if we're on the floor and ignore the floor collisions
-			if not entities[i].is_on_floor():
-				pass
+			# if not entities[i].is_on_floor():
+			# 	pass
 				# # Add a collision event to the entity that just collided to handle collisions
 				# var c_collision = C_Collision.new()
 				# var col = entities[i].get_last_slide_collision()
 				# c_collision.collision = col
 				# entities[i].add_component(c_collision)
 		# Set the velocity from the entity to the component
-		velocitys[i].velocity = entities[i].velocity
+		c_velocitys[i].velocity = entities[i].velocity
 		# Sync the transform back to the entity
-		Utils.sync_transform(entities[i])
+		c_trss[i].transform = entities[i].global_transform
+		
