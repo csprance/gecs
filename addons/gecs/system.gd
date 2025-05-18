2d6945e95c8681171f88ab5ca6a525a6fa83f9a8
Chris Sprance <chrissprance@gmail.com>
2025-05-18 10:09:02 -0400

    Refactor System class: improve formatting, add Runs enum for system execution order, and enhance query handling

--- addons/gecs/system.gd
+++ addons/gecs/system.gd
@@ -17,16 +17,27 @@
 ##         var velocity = entity.get_component(Velocity)
 ##         transform.position += velocity.direction * velocity.speed * delta
 ##[/codeblock]
-@icon('res://addons/gecs/assets/system.svg')
+@icon("res://addons/gecs/assets/system.svg")
 class_name System
 extends Node
 
+## These control when the system should run in relation to other systems.
+enum Runs {
+	## This system should run before all the systems defined in the array ex: [TransformSystem] means it will run before the [TransformSystem] system runs
+	Before,
+	## This system should run after all the systems defined in the array ex: [TransformSystem] means it will run after the [TransformSystem] system runs
+	After,
+}
+
 ## What group this system belongs to. Systems can be organized and run by group
-@export var group: String = ''
+@export var group: String = ""
 ## Determines whether the system should run even when there are no [Entity]s to process.
 @export var process_empty := false
 ## Is this system active. (Will be skipped if false)
-@export var active:= true
+@export var active := true
+
+## The order in which this system should run (Determined by kahns algorithm and the deps method Runs.Before and Runs.After deps)
+var order := 0
 
 ## Is this system paused. (Will be skipped if true)
 var paused := false
@@ -34,9 +45,20 @@ var paused := false
 ## The [QueryBuilder] object exposed for conveinence to use in the system and to create the query.
 var q: QueryBuilder
 
-var systemLogger = GECSLogger.new().domain('System')
+var systemLogger = GECSLogger.new().domain("System")
 
 var _using_subsystems = true
+
+
+## Override this method to define the [System]s that this system depends on.[br]
+## If not overriden the system will run based on the order of the systems in the [World] [br]
+## and the order of the systems in the [World] will be based on the order they were added to the [World].[br]
+func deps() -> Dictionary[int, Array]:
+	return {
+		Runs.After: [],
+		Runs.Before: [],
+	}
+
 
 ## Override this method and return a [QueryBuilder] to define the required [Component]s for the system.[br]
 ## If not overridden, the system will run on every update with no entities.
@@ -44,21 +66,28 @@ func query() -> QueryBuilder:
 	process_empty = true
 	return q
 
+
 ## Override this method to define any sub-systems that should be processed by this system.[br]
-func sub_systems():
-	_using_subsystems = false # If this method is not overridden then we are not using sub systems
+func sub_systems() -> Array[Array]:
+	_using_subsystems = false  # If this method is not overridden then we are not using sub systems
 	return []
+
 
 ## Runs once after the system has been added to the [World] to setup anything on the system one time[br]
 func setup():
 	pass
+
 
 ## The main processing function for the system.[br]
 ## This method should be overridden by subclasses to define the system's behavior.[br]
 ## [param entity] The [Entity] being processed.[br]
 ## [param delta] The time elapsed since the last frame.
 func process(entity: Entity, delta: float) -> void:
-	assert(false, "The 'process' method must be overridden in subclasses if it is not using sub systems.")
+	assert(
+		false,
+		"The 'process' method must be overridden in subclasses if it is not using sub systems."
+	)
+
 
 ## Sometimes you want to process all entities that match the system's query, this method does that.[br]
 ## This way instead of running one function for each entity you can run one function for all entities.[br]
@@ -79,6 +108,13 @@ func process_all(entities: Array, delta: float) -> bool:
 		entity.on_update(delta)
 	return did_run
 
+
+## Set the query builder to the systems q object.[br]
+func set_q():
+	if not q:
+		q = ECS.world.query
+
+
 ## handles the processing of all [Entity]s that match the system's query [Component]s.[br]
 ## [param delta] The time elapsed since the last frame.
 func _handle(delta: float):
@@ -86,22 +122,20 @@ func _handle(delta: float):
 		return
 	if _handle_subsystems(delta):
 		return
-	# Set our QueryBuilder RefCounted Object
-	q = ECS.world.query
+	set_q()
 	var did_run := false
 	# Query for the entities that match the system's query
 	var entities = query().execute()
 	did_run = process_all(entities, delta)
 	if did_run:
-		# Log the whole thing
-		_log_handle(entities)
 		entities.map(func(e): e.on_update(delta))
+
 
 func _handle_subsystems(delta: float):
 	var subsystems = sub_systems()
 	if not _using_subsystems:
 		return false
-	q = ECS.world.query
+	set_q()
 	var sub_systems_ran = false
 	for sub_sys_tuple in subsystems:
 		var did_run = false
@@ -117,14 +151,5 @@ func _handle_subsystems(delta: float):
 				did_run = true
 				sub_sys_process.call(entity, delta)
 		if did_run:
-			# Log the whole thing
-			_log_handle(entities)
 			entities.map(func(e): e.on_update(delta))
 	return sub_systems_ran
-
-func _log_handle(entities):
-	systemLogger.trace("""
-[%s]
-  -> Query: %s
-  -> Entities: %s
-""" % [self, q, entities])
